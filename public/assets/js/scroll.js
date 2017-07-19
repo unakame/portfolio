61c28e24b5f454e304ae53a7cb95a82b8e514a48
Dana Franco <turquoise.ilg@hotmail.com>
2017-07-18 19:53:07 -0500

    fixing colors

--- public/assets/js/scroll.js
+++ public/assets/js/scroll.js
@@ -4,8 +4,14 @@ window.addEventListener("scroll", function(){
   var currentScroll = window.scrollY;
   if (currentScroll < 100) {
   document.getElementById("ul").classList.remove("active");
+  document.getElementsByClassName("nav-info")[0].classList.remove("color-letters");
+  document.getElementsByClassName("nav-info")[1].classList.remove("color-letters");
+  document.getElementsByClassName("nav-info")[2].classList.remove("color-letters");
 } if (currentScroll > lastScrollTop){
   document.getElementById("ul").classList.add("active");
+  document.getElementsByClassName("nav-info")[0].classList.add("color-letters");
+  document.getElementsByClassName("nav-info")[1].classList.add("color-letters");
+  document.getElementsByClassName("nav-info")[2].classList.add("color-letters");
 } if( currentScroll >= 420 && currentScroll < 1160) {
   document.getElementById("ul").classList.add("active");
 } 
